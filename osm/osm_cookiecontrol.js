67e11d37659d35480fe31bde6b402a184a529231
Alphapi <axilpapa@gmail.com>
2018-10-18 10:51:07 +0300

    Include GTM code onAccept
    We should, also, remove those lines from their old position at the template.

--- osm/osm_cookiecontrol.js
+++ osm/osm_cookiecontrol.js
@@ -20,7 +20,13 @@ var config = {
 				label: 'Performance Cookies',
 				description: 'Performance cookies help us to improve our website by collecting and reporting information, on its usage.',
 				cookies: ['_dc_gtm_UA*','_ga','_gid'],
-				onAccept: function() {},
+				onAccept: function() {
+					(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+					'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+					})(window,document,'script','dataLayer','GTM-5GST4C');
+					},
 				onRevoke: function() {
 					// Disable Google Analytics
 					//window['ga-disable-UA-xxxxx'] = true;
